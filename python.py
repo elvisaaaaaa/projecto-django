344b207f5589b2ed2d1305decfae4947feba725c
elvisaaaaaa <131224917+elvisaaaaaa@users.noreply.github.com>
2024-06-19 16:34:25 -0400

    Add files via upload
    d2 . Tienda online

---
+++ python.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Order(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2) 
